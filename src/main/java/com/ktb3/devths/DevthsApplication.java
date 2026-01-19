e2853b81f654e5aecb78cf3bc79f54b246950129
Yun <mosy2266@naver.com>
2026-01-19 15:16:08 +0900

    first commit

---
+++ src/main/java/com/ktb3/devths/DevthsApplication.java
@@ -0,0 +1,11 @@
+package com.ktb3.devths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevthsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DevthsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ktb3/devths/DevthsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb3.devths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevthsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
