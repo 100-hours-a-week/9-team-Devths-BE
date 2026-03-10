304c3c008cf19ec48515865b11602d60fb70144d
Yun <mosy2266@naver.com>
2026-03-10 14:49:48 +0900

    feat: FCM 푸시 알림 발송 로직 구현 (#252)

--- src/main/java/com/ktb3/devths/notification/repository/FcmTokenRepository.java
+++ src/main/java/com/ktb3/devths/notification/repository/FcmTokenRepository.java
@@ -1,5 +1,6 @@
 package com.ktb3.devths.notification.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,8 @@ public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
 	Optional<FcmToken> findByUserIdAndDeviceId(Long userId, String deviceId);
 
 	Optional<FcmToken> findByDeviceId(String deviceId);
+
+	List<FcmToken> findAllByUserIdAndIsActiveTrue(Long userId);
+
+	void deleteByToken(String token);
 }
